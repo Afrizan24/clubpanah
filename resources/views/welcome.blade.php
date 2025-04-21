9ab91162e4c8107bec4281736a2c7194fb79429b
izanbecomepro <afrizanmaulana0@gmail.com>
2025-04-21 12:03:32 +0700

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-
     <title>CLub Panah</title>
 </head>
-
-<body
-    class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     awdjhjkahwda
 </body>
 </html>
