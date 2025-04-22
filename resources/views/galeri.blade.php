b42cb30690f4b3b5b21a107088d1f5595acac28d
aldippppp <aldippppp@gmail.com>
2025-04-22 16:02:09 +0700

    .

---
+++ resources/views/galeri.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <title>GAS</title>
+    
+</head>
+
+<body>
+    
+    <x-navbar></x-navbar>
+
+    <x-galeri></x-galeri>
+
+    <x-footer></x-footer>
+
+</body>
+
+</html>
